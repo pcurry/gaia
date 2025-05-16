7ebaad2d82822ea9a83bfa0885cc1d679f358d4b
tcdoggett <tcdoggett@gmail.com>
2025-05-16 18:04:23 -0400

    Update Gaia_Query.py
    add Sol to list of stars too bright for Gaia

--- Gaia_Query.py
+++ Gaia_Query.py
@@ -21,14 +21,14 @@ df.insert(6, "Y (parsecs)", (df.iloc[:,4]*np.cos(np.radians(df.iloc[:,2])))*np.s
 #create new column with calculated z-coordinates (where positive z is "galactic north" and negative z is "galactic south" per arbitrary convention)
 df.insert(7, "Z (parsecs)", (df.iloc[:,4]*np.sin(np.radians(df.iloc[:,2]))), allow_duplicates=True)
 #add stars which were too bright for Gaia (due to detector saturation)
-bright_stars = {'designation': ["Alpheratz", "Mirach", "Almach"], 
-'l': [111.7322278, 127.1075879, 136.9645847], 
-'b': [-32.84335675, -27.10282071, -18.55901423], 
-'parallax': [33.62, 16.52, 8.3], 
-'Distance (parsecs)': [29.74419988, 60.53268765, 120.4819277], 
-'X (parsecs)': [-9.252949243, -32.50992551, -83.4844413], 
-'Y (parsecs)': [23.21361869, 42.9739963, 77.9470326], 
-'Z (parsecs)': [-16.13159212, -27.57801044, -38.34713907]}
+bright_stars = {'designation': ["Sol", "Alpheratz", "Mirach", "Almach"], 
+'l': [0, 111.7322278, 127.1075879, 136.9645847], 
+'b': [0, -32.84335675, -27.10282071, -18.55901423], 
+'parallax': [8800, 33.62, 16.52, 8.3], 
+'Distance (parsecs)': [0, 29.74419988, 60.53268765, 120.4819277], 
+'X (parsecs)': [0, -9.252949243, -32.50992551, -83.4844413], 
+'Y (parsecs)': [0, 23.21361869, 42.9739963, 77.9470326], 
+'Z (parsecs)': [0, -16.13159212, -27.57801044, -38.34713907]}
 bright_df = pd.DataFrame(bright_stars)
 df_combined = pd.concat([df, bright_df], ignore_index=True)
 print(df_combined)
