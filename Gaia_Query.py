35562f3157f2745f9985a24a930cdeb86c5dc2ed
tcdoggett <tcdoggett@gmail.com>
2025-05-17 21:10:20 -0400

    Update Gaia_Query.py
    move ingest of bright stars and df concat to before x,y,z calculation

--- Gaia_Query.py
+++ Gaia_Query.py
@@ -1,4 +1,4 @@
-#pulls Gaia data based on distance from solar system and converts to sol-centric (x,y,z) coordinates
+#this script pulls Gaia data based on distance from the solar system and converts to sol-centric (x,y,z) coordinates
 import numpy as np
 from astroquery.gaia import Gaia
 from astroquery.simbad import Simbad
@@ -15,26 +15,25 @@ job = Gaia.launch_job_async(query)
 table = job.get_results()
 #%%
 df = table.to_pandas()
-#create new column and calculate distance to star (in parsecs) based on Gaia parallax
-p=df["parallax"]
-d=df["distance"]=1/p*1000
-l=np.radians(df["l"])
-b=np.radians(df["b"])
-#create new column with calculated x-coordinates (where positive x is coreward and negative x is rimward)
-df["X"]=d*np.cos(b)*np.cos(l)
-#create new column with calculated y-coordinates (where positive y is spinward and negative y is trailing in context of the direction of rotaiton of the galactic disk)
-df["Y"]=d*np.cos(b)*np.sin(l)
-#create new column with calculated z-coordinates (where positive z is "galactic north" and negative z is "galactic south" per arbitrary convention)
-df["Z"]=d*np.sin(b)
-#add stars which were too bright for Gaia (due to detector saturation)
-bright_stars = {'designation': ["Sol", "Alpheratz", "Mirach", "Almach"], 
-'l': [0, 111.7322278, 127.1075879, 136.9645847], 
-'b': [0, -32.84335675, -27.10282071, -18.55901423], 
-'parallax': [8800, 33.62, 16.52, 8.3], 
-'distance': [0, 29.74419988, 60.53268765, 120.4819277], 
-'X': [0, -9.252949243, -32.50992551, -83.4844413], 
-'Y': [0, 23.21361869, 42.9739963, 77.9470326], 
-'Z': [0, -16.13159212, -27.57801044, -38.34713907]}
+#add stars which were too bright for Gaia (e.g. due to detector saturation)
+bright_stars = {
+    'designation': ["Sol", "Alpha Centauri A", "Alpha Centauri B", "Sirius", "Procyon"], 
+    'l': [0, 315.7341713, 315.7300838, 227.2302913, 213.7022414], 
+    'b': [0, -0.67965625, -0.68171325, -8.89028121, 13.01939682],
+    'parallax': [8800, 742.12, 742.12, 379.21, 284.56]}
 bright_df = pd.DataFrame(bright_stars)
 df_combined = pd.concat([df, bright_df], ignore_index=True)
+#create new column and calculate distance to star (in parsecs) based on Gaia parallax
+p=df_combined["parallax"]
+d=df_combined["distance"]=1/p*1000
+#convert galactic coordinates in Gaia from degrees to radians
+l=np.radians(df_combined["l"])
+b=np.radians(df_combined["b"])
+#create new column with calculated x-coordinates (where positive x is coreward and negative x is rimward)
+df_combined["X"]=d*np.cos(b)*np.cos(l)
+#create new column with calculated y-coordinates (where positive y is spinward and negative y is trailing in context of the direction of rotaiton of the galactic disk)
+df_combined["Y"]=d*np.cos(b)*np.sin(l)
+#create new column with calculated z-coordinates (where positive z is "galactic north" and negative z is "galactic south" per arbitrary convention)
+df_combined["Z"]=d*np.sin(b)
 print(df_combined)
+# %%
