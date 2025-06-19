7a060d86e1aa36923da4d35bd50d4259e45a6184
tcdoggett <tcdoggett@gmail.com>
2025-06-18 22:19:01 -0400

    Update system_generator.py

--- system_generator.py
+++ system_generator.py
@@ -1,1 +1,10 @@
 #script to procedurally generate realistic (but fictional) star systems using outputs from sector_querry.py
+
+# Step One: Primary Star Mass
+# Step Two: Stellar Multiplicity
+# Step Three: Arrange Components
+# Step Four: Star System Age
+# Step Five: Star System Metallicity
+# Step Six: Stellar Evolution
+# Step Seven: Stellar Classification
+# Step Eight: Stellar Orbital Parameters
