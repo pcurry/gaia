e89a568ecaf57fcd976305afd496f1b8f6638272
tcdoggett <tcdoggett@gmail.com>
2025-06-19 22:31:18 -0400

    Update system_generator.py

--- system_generator.py
+++ system_generator.py
@@ -1,10 +1,34 @@
-#script to procedurally generate realistic (but fictional) star systems using outputs from sector_querry.py
+# script to procedurally generate realistic (but fictional) star systems using outputs from sector_querry.py
+# reference: Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
 
-# Step One: Primary Star Mass
-# Step Two: Stellar Multiplicity
-# Step Three: Arrange Components
-# Step Four: Star System Age
-# Step Five: Star System Metallicity
-# Step Six: Stellar Evolution
-# Step Seven: Stellar Classification
-# Step Eight: Stellar Orbital Parameters
+# Step 1: Primary Star Mass
+# Step 2: Stellar Multiplicity
+# Step 3: Arrange Components
+# Step 4: Star System Age
+# Step 5: Star System Metallicity
+# Step 6: Stellar Evolution
+# Step 7: Stellar Classification
+# Step 8: Stellar Orbital Parameters
+# Step 9: Protoplanetary Disk
+# Step 10: Disk Instability
+# Step 11: Core Accretion
+# Step 12: Oligarchic Collision
+# Step 13: Planetary Orbital Radii
+# Step 14: Planetary Mass
+# Step 15: Orbital Eccentricity
+# Step 16: Physical Parameters
+# Step 17: Natural Satellites
+# Step 18: Orbital Period
+# Step 19: Rotation Period
+# Step 20: Obliquity
+# Step 21: Local Calendar
+# Step 22: Blackbody Temperature
+# Step 23: Water
+# Step 24: Geophysics
+# Step 25: Magnetic Field
+# Step 26: Early Atmosphere
+# Step 27: Albedo
+# Step 28: Carbon Dioxide
+# Step 29: Presence of Life
+# Step 30: Average Surface Temperature
+# Step 31: Finalize Atmosphere
