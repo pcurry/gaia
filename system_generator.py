fef625ad1dcb867639b4c36c5413c6cb0362da01
tcdoggett <tcdoggett@gmail.com>
2025-06-23 23:31:52 -0400

    Update system_generator.py

--- system_generator.py
+++ system_generator.py
@@ -6,18 +6,12 @@
 # Step 3: Arrange Components
 # Step 4: Star System Age
 
-# Roll (d100) / Population / Base Age / Age Range
-# 01-42 / Young Population I / 0.0 / 2.0
-# 43-76 / Intermediate Population I / 2.0 / 3.0
-# 77-95 / Old Population I / 5.0 / 3.0
-# 96-99 / Disk Population II / 8.0 / 1.5
-# 00 / Halo Population II / 9.5 / 3.0
 random_integer = random.randint(1, 100)
-if 1 <= random_integer <= 42, then Population="Young Population I"
-if 43 <= random_integer <= 76, then Population="Intermediate Population"
-if 77 <= random_integer <= 95, then Population="Old Population I"
-if 96 <= random_integer <= 99, then Population="Disk Population II"
-if 100 = random_integer, then Populattion="Halo Population II"
+if 1 <= random_integer <= 42, then Population="Young Population I" and Base_Age="0.0" and Age_Range="2.0"
+if 43 <= random_integer <= 76, then Population="Intermediate Population" and Base_Age="2.0" and Age_Range="1.0"
+if 77 <= random_integer <= 95, then Population="Old Population I" and Base_Age="5.0" and Age_Range="3.0"
+if 96 <= random_integer <= 99, then Population="Disk Population II" and Base_Age="8.0" and Age_Range="1.5"
+if 100 = random_integer, then Populattion="Halo Population II" and Base_Age="9.5" and Age_Range="3.0"
 
 # Step 5: Star System Metallicity
 # Step 6: Stellar Evolution
