e2e6e130de09bdfc29a25062aa75220d8e18984b
tcdoggett <tcdoggett@gmail.com>
2025-07-03 20:55:20 -0400

    add mass tables for other star types

--- system_generator.py
+++ system_generator.py
@@ -30,15 +30,161 @@ if 1 <= random_integer <= 3:
         Mass = 0.07
 elif 4 <= random_integer <=77:
     Category = "Low Mass Star"
-    #input routine for calculating mass of this category of stars
+    random_integer = random.randint(1,100)
+    if 1 <= random_integer <= 13:
+        Mass = 0.08
+    elif 14 <= random_integer <= 23:
+        Mass = 0.10
+    elif 24 <= random_integer <= 34:
+        Mass = 0.12
+    elif 35 <= random_integer <= 43:
+        Mass = 0.15
+    elif 44 <= random_integer <= 52:
+        Mass = 0.18
+    elif 53 <= random_integer <= 59:
+        Mass = 0.22
+    elif 60 <= random_integer <= 65:
+        Mass = 0.26
+    elif 66 <= random_integer <= 70:
+        Mass = 0.30
+    elif 71 <= random_integer <= 74:
+        Mass = 0.34
+    elif 75 <= random_integer <= 77:
+        Mass = 0.38
+    elif 78 <= random_integer <= 80:
+        Mass = 0.42
+    elif 81 <= random_integer <= 83:
+        Mass = 0.46
+    elif 84 <= random_integer <= 86:
+        Mass = 0.50
+    elif 87 <= random_integer <= 89:
+        Mass = 0.53
+    elif 90 <= random_integer <= 92:
+        Mass = 0.56
+    elif 93 <= random_integer <= 95:
+        Mass = 0.59
+    elif 96 <= random_integer <= 97:
+        Mass = 0.62
+    elif 98 <= random_integer <= 99:
+        Mass = 0.65
+    elif 100 == random_integer:
+        Mass = 0.68
 elif 78 <= random_integer <=90:
     Category = "Intermediate Mass Star"
-    #input routine for calculating mass of this category of stars
+    random_integer = random.randint(1,100)
+    if 1 <= random_integer <= 7:
+        Mass = 0.70
+    elif 8 <= random_integer <= 13:
+        Mass = 0.72
+    elif 14 <= random_integer <= 19:
+        Mass = 0.74
+    elif 20 <= random_integer <= 24:
+        Mass = 0.76
+    elif 25 <= random_integer <= 29:
+        Mass = 0.78
+    elif 30 <= random_integer <= 34:
+        Mass = 0.80
+    elif 35 <= random_integer <= 39:
+        Mass = 0.82
+    elif 40 <= random_integer <= 43:
+        Mass = 0.84
+    elif 44 <= random_integer <= 47:
+        Mass = 0.86
+    elif 48 <= random_integer <= 51:
+        Mass = 0.88
+    elif 52 <= random_integer <= 55:
+        Mass = 0.90
+    elif 56 <= random_integer <= 59:
+        Mass = 0.92
+    elif 60 <= random_integer <= 62:
+        Mass = 0.94
+    elif 63 <= random_integer <= 65:
+        Mass = 0.96
+    elif 66 <= random_integer <= 68:
+        Mass = 0.98
+    elif 69 <= random_integer <= 71:
+        Mass = 1.00
+    elif 72 <= random_integer <= 74:
+        Mass = 1.02
+    elif 75 <= random_integer <= 78:
+        Mass = 1.04
+    elif 79 <= random_integer <= 82:
+        Mass = 1.07
+    elif 83 <= random_integer <= 85:
+        Mass = 1.10
+    elif 86 <= random_integer <= 89:
+        Mass = 1.13
+    elif 90 <= random_integer <= 92:
+        Mass = 1.16
+    elif 93 <= random_integer <= 95:
+        Mass = 1.19
+    elif 96 <= random_integer <= 97:
+        Mass = 1.22
+    elif 98 <= random_integer <= 100:
+        Mass = 1.25
 elif 91 <= random_integer <=100:
     Category = "High Mass Star"
-    #input routine for calculating mass of this category of stars
+    random_integer = random.randint(1,100)
+    if 1 <= random_integer <= 3:
+        Mass = 1.28
+    elif 4 <= random_integer <= 6:
+        Mass = 1.31
+    elif 7 <= random_integer <= 9:
+        Mass = 1.34
+    elif 10 <= random_integer <= 12:
+        Mass = 1.37
+    elif 13 <= random_integer <= 16:
+        Mass = 1.40
+    elif 17 <= random_integer <= 19:
+        Mass = 1.44
+    elif 20 <= random_integer <= 23:
+        Mass = 1.48
+    elif 24 <= random_integer <= 27:
+        Mass = 1.53
+    elif 28 <= random_integer <= 31:
+        Mass = 1.58
+    elif 32 <= random_integer <= 35:
+        Mass = 1.64
+    elif 36 <= random_integer <= 38:
+        Mass = 1.70
+    elif 39 <= random_integer <= 41:
+        Mass = 1.76
+    elif 42 <= random_integer <= 45:
+        Mass = 1.82
+    elif 46 <= random_integer <= 49:
+        Mass = 1.90
+    elif 50 <= random_integer <= 53:
+        Mass = 2.00
+    elif 54 <= random_integer <= 56:
+        Mass = 2.10
+    elif 57 <= random_integer <= 59:
+        Mass = 2.20
+    elif 60 <= random_integer <= 62:
+        Mass = 2.30
+    elif 63 <= random_integer <= 67:
+        Mass = 2.40
+    elif 68 <= random_integer <= 71:
+        Mass = 2.60
+    elif 72 <= random_integer <= 75:
+        Mass = 2.80
+    elif 76 <= random_integer <= 78:
+        Mass = 3.00
+    elif 79 <= random_integer <= 82:
+        Mass = 3.20
+    elif 83 <= random_integer <= 87:
+        Mass = 3.50
+    elif 88 <= random_integer <= 91:
+        Mass = 4.00
+    elif 92 <= random_integer <= 94:
+        Mass = 4.50
+    elif 95 <= random_integer <= 96:
+        Mass = 5.00
+    elif 97 <= random_integer <= 98:
+        Mass = 5.50
+    elif 99 <= random_integer <= 100:
+        Mass = 6.00
 print(f"Star Type: {Category}")
-print(f"Mass: {Mass}")
+print(f"Mass: {Mass} solar masses")
 
 # Step 2: Stellar Multiplicity
 # Step 3: Arrange Components
@@ -92,9 +238,6 @@ if Metallicity < 0:
 print(f"Metallicity: {Metallicity}")
 
 # Step 6: Stellar Evolution
-
-
-
 # Step 7: Stellar Classification
 # Step 8: Stellar Orbital Parameters
 # Step 9: Protoplanetary Disk
