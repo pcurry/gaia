4810f6b52436d18dfeaf16ae95147479411c01c8
tcdoggett <tcdoggett@gmail.com>
2025-07-14 08:58:01 -0400

    Update system_generator.py

--- system_generator.py
+++ system_generator.py
@@ -298,9 +298,9 @@ elif Number_of_Stars <= 2:
         Mass_Ratio = 0.85
     elif 77 <= roll_for_binary_mass_ratio <= 86:
         Mass_Ratio = 0.90
-    elif roll_for_binary_mass_ratio >= 88:
+    elif 87 <= roll_for_binary_mass_ratio <= 100:
         Mass_Ratio = 0.95
-    print(f"Mass Ratio: {Mass_Ratio}")
+    print(f"Mass Ratio Between Star A and Star B: {Mass_Ratio}")
     Mass_B = Mass * Mass_Ratio
     if Mass_B < 0.015:
         Mass_B = 0.015
@@ -419,7 +419,7 @@ elif Number_of_Stars == 3:
         if Mass_B < 0.015:
             Mass_B = 0.015
         print(f"Mass of Star B: {Mass_B} solar masses")
-        roll_for_AC_mass_ratio = d100
+        roll_for_AC_mass_ratio = d100()
         if roll_for_AC_mass_ratio <= 4:
             Mass_Ratio = 0.05
         elif 5 <= roll_for_AC_mass_ratio <= 8:
@@ -456,7 +456,7 @@ elif Number_of_Stars == 3:
             Mass_Ratio = 0.85
         elif 77 <= roll_for_AC_mass_ratio <= 86:
             Mass_Ratio = 0.90
-        elif roll_for_BC_mass_ratio >= 88:
+        elif roll_for_AC_mass_ratio >= 88:
             Mass_Ratio = 0.95
         print(f"Mass Ratio Between Star A and Star C: {Mass_Ratio}")
         Mass_C = Mass * Mass_Ratio
@@ -466,8 +466,128 @@ elif Number_of_Stars == 3:
 
 elif Number_of_Stars == 4:
     Stellar_Arrangement = "AB-CD"
-    print("Quadruple Star System - AB close pair and distant CD pair")
-    print("need to write more code for this")
+    print("Quadruple Star System - two binary sytems AB and CD with wide separation")
+
+    roll_for_AB_mass_ratio = d100()
+    if roll_for_AB_mass_ratio <= 4:
+        Mass_Ratio = 0.05
+    elif 5 <= roll_for_AB_mass_ratio <= 8:
+        Mass_Ratio = 0.10
+    elif 9 <= roll_for_AB_mass_ratio <= 12:
+        Mass_Ratio = 0.15
+    elif 13 <= roll_for_AB_mass_ratio <= 16:
+        Mass_Ratio = 0.20
+    elif 17 <= roll_for_AB_mass_ratio <= 20:
+        Mass_Ratio = 0.25
+    elif 21 <= roll_for_AB_mass_ratio <= 24:
+        Mass_Ratio = 0.30
+    elif 25 <= roll_for_AB_mass_ratio <= 28:
+        Mass_Ratio = 0.35
+    elif 29 <= roll_for_AB_mass_ratio <= 31:
+        Mass_Ratio = 0.40
+    elif 32 <= roll_for_AB_mass_ratio <= 34:
+        Mass_Ratio = 0.45
+    elif 35 <= roll_for_AB_mass_ratio <= 38:
+        Mass_Ratio = 0.50
+    elif 39 <= roll_for_AB_mass_ratio <= 43:
+        Mass_Ratio = 0.55
+    elif 44 <= roll_for_AB_mass_ratio <= 48:
+        Mass_Ratio = 0.60
+    elif 49 <= roll_for_AB_mass_ratio <= 53:
+        Mass_Ratio = 0.65
+    elif 54 <= roll_for_AB_mass_ratio <= 58:
+        Mass_Ratio = 0.70
+    elif 59 <= roll_for_AB_mass_ratio <= 63:
+        Mass_Ratio = 0.75
+    elif 64 <= roll_for_AB_mass_ratio <= 69:
+        Mass_Ratio = 0.80
+    elif 70 <= roll_for_AB_mass_ratio <= 76:
+        Mass_Ratio = 0.85
+    elif 77 <= roll_for_AB_mass_ratio <= 86:
+        Mass_Ratio = 0.90
+    elif roll_for_AB_mass_ratio >= 88:
+        Mass_Ratio = 0.95
+    print(f"Mass Ratio Between Star A and Star B: {Mass_Ratio}")
+    Mass_B = Mass * Mass_Ratio
+    if Mass_B < 0.015:
+        Mass_B = 0.015
+    print(f"Mass of Star B: {Mass_B} solar masses")
+
+    roll_for_AC_mass_ratio = d100()
+    if roll_for_AC_mass_ratio <= 4:
+        Mass_Ratio = 0.05
+    elif 5 <= roll_for_AC_mass_ratio <= 8:
+        Mass_Ratio = 0.10
+    elif 9 <= roll_for_AC_mass_ratio <= 12:
+        Mass_Ratio = 0.15
+    elif 13 <= roll_for_AC_mass_ratio <= 16:
+        Mass_Ratio = 0.20
+    elif 17 <= roll_for_AC_mass_ratio <= 20:
+        Mass_Ratio = 0.25
+    elif 21 <= roll_for_AC_mass_ratio <= 24:
+        Mass_Ratio = 0.30
+    elif 25 <= roll_for_AC_mass_ratio <= 28:
+        Mass_Ratio = 0.35
+    elif 29 <= roll_for_AC_mass_ratio <= 31:
+        Mass_Ratio = 0.40
+    elif 32 <= roll_for_AC_mass_ratio <= 34:
+        Mass_Ratio = 0.45
+    elif 35 <= roll_for_AC_mass_ratio <= 38:
+        Mass_Ratio = 0.50
+    elif 39 <= roll_for_AC_mass_ratio <= 43:
+        Mass_Ratio = 0.55
+    elif 44 <= roll_for_AC_mass_ratio <= 48:
+        Mass_Ratio = 0.60
+    elif 49 <= roll_for_AC_mass_ratio <= 53:
+        Mass_Ratio = 0.65
+    elif 54 <= roll_for_AC_mass_ratio <= 58:
+        Mass_Ratio = 0.70
+    elif 59 <= roll_for_AC_mass_ratio <= 63:
+        Mass_Ratio = 0.75
+    elif 64 <= roll_for_AC_mass_ratio <= 69:
+        Mass_Ratio = 0.80
+    elif 70 <= roll_for_AC_mass_ratio <= 76:
+        Mass_Ratio = 0.85
+    elif 77 <= roll_for_AC_mass_ratio <= 86:
+        Mass_Ratio = 0.90
+    elif roll_for_AB_mass_ratio >= 88:
+        Mass_Ratio = 0.95
+    print(f"Mass Ratio Between Star A and Star C: {Mass_Ratio}")
+    Mass_C = Mass * Mass_Ratio
+    if Mass_C < 0.015:
+        Mass_C = 0.015
+    print(f"Mass of Star C: {Mass_C} solar masses")
+
+    roll_for_CD_mass_ratio = (d100()+30)
+    if roll_for_CD_mass_ratio == 31:
+        Mass_Ratio = 0.40
+    elif 32 <= roll_for_CD_mass_ratio <= 34:
+        Mass_Ratio = 0.45
+    elif 35 <= roll_for_CD_mass_ratio <= 38:
+        Mass_Ratio = 0.50
+    elif 39 <= roll_for_CD_mass_ratio <= 43:
+        Mass_Ratio = 0.55
+    elif 44 <= roll_for_CD_mass_ratio <= 48:
+        Mass_Ratio = 0.60
+    elif 49 <= roll_for_CD_mass_ratio <= 53:
+        Mass_Ratio = 0.65
+    elif 54 <= roll_for_CD_mass_ratio <= 58:
+        Mass_Ratio = 0.70
+    elif 59 <= roll_for_CD_mass_ratio <= 63:
+        Mass_Ratio = 0.75
+    elif 64 <= roll_for_CD_mass_ratio <= 69:
+        Mass_Ratio = 0.80
+    elif 70 <= roll_for_CD_mass_ratio <= 76:
+        Mass_Ratio = 0.85
+    elif 77 <= roll_for_CD_mass_ratio <= 86:
+        Mass_Ratio = 0.90
+    elif 87 <= roll_for_AB_mass_ratio <= 130:
+        Mass_Ratio = 0.95
+    print(f"Mass Ratio Between Star C and Star D: {Mass_Ratio}")
+    Mass_D = Mass_C * Mass_Ratio
+    if Mass_D < 0.015:
+        Mass_D = 0.015
+    print(f"Mass of Star D: {Mass_D} solar masses")
 
 # Step 4: Star System Age
 
