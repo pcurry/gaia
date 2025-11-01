0bebd969955a701f2a273666c239bb4c3753045d
tcdoggett <tcdoggett@gmail.com>
2025-10-31 22:43:50 -0400

    tweaks to oligarchic collisions

--- src/planetary_system_data/oligarchic_collision.py
+++ src/planetary_system_data/oligarchic_collision.py
@@ -1,2 +1,13 @@
 # this sub-program covers step 12 ("Oligarchic Collision") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
-# input Step 12 elements - starting from pg. 67
+
+# "The small, rocky planets of the inner system are the last to form, taking tens of millions of years to reach their final state. Inside the formation ice line, the dry planetesimals clump, forming a
+# swarm of millions of planetoids. Later in the process, these planetoids coalesce and form a few dozen oligarchs, Mars-sized objects that take up a wide variety of orbits throughout the inner disk. Finally, 
+# these oligarchs in turn combine to form full-sized rocky planets. This process is chaotic and often extremely violent, involving many collisions and drastic changes in orbital parameters.
+# This step places any planets (and related objects) which form by oligarchic collision in the evolving planetary system."
+
+# Procedure - To begin, check to see whether either formation orbit 0, or the next formation orbit outward, is already occupied by a core-accretion planet as determined in Step Eleven. If so, then no planets
+# will form in this step; skip forward to Step Thirteen"
+
+# WRITE CODE HERE
+
+# continue input Step 12 elements - from pg. 67
