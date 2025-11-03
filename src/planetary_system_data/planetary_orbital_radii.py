830fc98647b33d39730b57b8dc0a8da125e1d3b6
tcdoggett <tcdoggett@gmail.com>
2025-11-02 20:03:26 -0500

    Update planetary_orbital_radii.py

--- src/planetary_system_data/planetary_orbital_radii.py
+++ src/planetary_system_data/planetary_orbital_radii.py
@@ -1,1 +1,33 @@
-# this sub-program covers step 13 ("Planetary Orbital Radii") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
+# this sub-program covers step 13 ("Planetary Orbital Radii") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction" - starting on pg. 69
+
+# "At this point, the worksheet should indicate how many planets there are after the planetary system has settled down to a long-term-stable state. Each planet should be associated with its
+# own formation orbit, although that is not necessarily the position where it began the process of planetary formation. Some formation orbits may have been removed from consideration, or
+# will simply be empty."
+
+# "In this step, we assign the final orbital radius for each planet; the resulting orbits will be stable over billion-year timescales."
+
+# "In this step, ignore the radii of the various formation orbits, unless specifically indicated. Final planetary orbital radii are subject to multiple variables"
+
+# "If no planets have survived the process of planetary formation (usually due to a close-in forbidden zone) then the design process for this star and its planetary system is complete"
+
+# "Carry out the following sub-steps in order, recording the final orbital radius for each planet"
+
+# Sub-Step 13-1: Determine Orbital Radius of Innermost Planet (pg.70)
+
+# INSERT CODE HERE
+
+# Sub-Step 13-2: Estimate Orbital Radius of Outermost Planet (pg.70)
+
+# INSERT CODE HERE
+
+# Sub-Step 13-3: Determine Expected Ratio of Orbital Radii (pg. 71)
+
+# INSERT CODE HERE
+
+# Sub-Step 13-4: Determine Remaining Orbital Radii (pg. 71)
+
+# INSERT CODE HERE
+
+# Sub-Step 13-5: Check for Special Cases (pg. 72)
+
+# INSERT CODE HERE
