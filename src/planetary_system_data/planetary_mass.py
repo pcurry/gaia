916ee4bafdc4c69d2de054a1f55336c82c86a793
tcdoggett <tcdoggett@gmail.com>
2025-11-03 21:11:30 -0500

    Update planetary_mass.py

--- src/planetary_system_data/planetary_mass.py
+++ src/planetary_system_data/planetary_mass.py
@@ -1,1 +1,68 @@
-# this sub-program covers step 14 ("Planetary Mass") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
+# this sub-program covers step 14 ("Planetary Mass") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction" starting on pg. 75
+
+# "This step begins to characterize the physical properties of each planet in the planetary system. In particular, it determines the overall category of each planet and its total mass. In general,
+# round planetary masses to two significant figures"
+
+# "Procedure - Each of the three mechanisms for planetary formation (disk instability, core accretion, and terrestrial planet formation) will produce planets of differing mass and with different
+# properties. For each planet in the system, determine the planet’s category and mass in accordance with its formation mechanism."
+
+# Disk Instability Planets
+# "Planets formed by disk instability will always be gas giants. To generate a plausible figure for such a planet’s mass, use the appropriate formula below (depending on the mass of the primary star) with 
+# a roll of 3d6:"
+
+# (codify equations from pg. 76)
+
+# "M<sub>P</sub> is the mass of the gas giant in Earth-masses, M is the mass of the primary star in solar masses, and D is the disk mass factor determined in Step Nine. Vary the result by up to 10%,
+# but gas giants have a minimum of 5 Earth-masses, and a maximum of 5,000 Earth-masses."
+
+# Core Accretion Planets
+
+# "Planets formed by core accretion may be either failed cores or gas giants. Their eventual mass depends on how efficiently they grow and begin to accumulate hydrogen and helium gas in the
+# early stages of planetary formation."
+
+# "For each planet formed by core accretion, begin by determining the mass of the planetary core, the portion of the planet that is composed of stony or icy materials rather than light gases:"
+
+# (codify equation from pg. 76)
+
+# "M<sub>C</sub> is the mass of the planetary core in Earth-masses, and P is the planetary system’s planetesimal mass as determined in Step Nine. K is a constant that depends on which core-
+# accretion planet is being evaluated: 20 for the innermost core-accretion planet, 10 for the next-innermost, and 4 for all others."
+
+# "If the mass of the planetary core is less than 5 Earth-masses, the planet is automatically a failed core, and its total mass is equal to that of the planetary core. Very this mass by up to 10%, but
+# failed cores have maximum mass of 5 Earth-masses"
+
+# "If the planet is not a failed core, roll 3d6 on the Core Accretion Mass Table. Use the column indicated for the innermost, the next-innermost, or any subsequent core-accretion planets. Core-accretion planets
+# that turn out to be failed cores do count against this total."
+
+# "Multiply the planet’s planetesimal mass by the Accretion Factor from the table. The result is the gas giant’s total mass, in Earth-masses. Vary the result by up to 10%, but gas giants have a minimum of 5 Earth-masses,
+# and a maximum of 5,000 Earth-masses."
+
+# (codify the Core Accretion Mass Table on pg. 76)
+
+# Oligarchic Collision Planets
+
+# "Planets formed by oligarchic collision inside the formation ice line will be small, rocky worlds. Most will be terrestrial planets, although some may be leftover oligarchs or even planetoid belts"
+
+# "If there is more than one oligarchic-collision planet, the innermost such planet may be a leftover oligarch regardless of the available mass for planetary formation. Roll 3d6, and on a 12
+# or higher the innermost planet will be a leftover oligarch"
+
+# "Otherwise, for each planet formed by oligarchic collision, begin by estimating the planet’s mass:"
+
+# (codify equation from top of pg. 77)
+
+# "M is the planet’s estimated mass in Earth-masses, and P is the system’s planetesimal mass as determined in Step Nine."
+
+# "If at least one planet formed by core accretion, multiply the estimated mass by the planetesimal mass factor generated in Step Eleven. This is based on how many formation orbits the migrating planet crossed 
+# as it moved inwards during planetary formation."
+
+# "If the current planet’s formation orbit was depleted due to the migration of a core-accretion planet, further multiply the estimated mass by 0.1. This will only happen if a Grand Tack event took place, 
+# and only for planets close to the migrating planet's final orbit."
+
+# "If the final estimated mass is greater than 0.18 Earth-masses, the result is a terrestrial planet with the indicated mass. Terrestrial planets have a minimum of 0.18 Earth-masses, and no maximum mass"
+
+# "If the final estimated mass is 0.18 Earth-masses or less, and the planet’s orbit is adjacent to either a core-accretion planet or a forbidden zone, then the result is a planetoid belt of negligible mass."
+
+# "Otherwise, the result is a leftover oligarch. Regardless of how a leftover oligarch is placed, determine its mass by"
+
+# (codify equation from middle of pg. 77)
+
+# "M is the planet’s mass in Earth-masses"
