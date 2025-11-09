0d3296913f5da92bdad2b89b8906bf821411ad5f
tcdoggett <tcdoggett@gmail.com>
2025-11-09 14:23:57 -0500

    Update obliquity.py

--- src/planetary_system_data/obliquity.py
+++ src/planetary_system_data/obliquity.py
@@ -1,1 +1,59 @@
 # this sub-program covers step 20 ("Obliquity") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
+# starting from page 93
+
+# "The obliquity of an object is the angle between its rotational axis and its orbital axis, or equivalently the angle between its equatorial plane and its 
+# orbital plane. It’s often colloquially called the axial tilt of a moon or planet. Obliquity can have significant effects on the surface conditions of a world, 
+# affecting daily and seasonal variations in temperature."
+
+# "Procedure"
+
+# "A world will fall into one of three categories: a major satellite of a planet, a planet with its own major satellite, or a planet without major satellites. 
+# These are the same three cases used in Step Nineteen."
+
+# "First Case: Major Satellites of Planets"
+
+# "Major satellites of planets, as placed in Step Seventeen, will have little or no obliquity with respect to the planet’s orbital plane. To determine the obliquity of 
+# such a satellite at random, roll 3d6-8 (minimum 0) and take the result as the obliquity in degrees."
+
+# "Note that the major satellites of gas giants, distant from their primary star, may be an exception to this general rule. For example, in our own planetary system, 
+# the planet Uranus is tilted at almost 90 degrees to its orbital plane. Its satellites all orbit close to the equatorial plane of Uranus, so their orbits are also at a 
+# large angle, and their obliquity with respect to the planet’s orbital plane is very high. Cases like this are unlikely for the smaller planets close to a primary
+# star—tidal interactions will quickly “flatten” the orbital planes of any major satellites there."
+
+# "Second Case: Planets with Major Satellites"
+
+# "A leftover oligarch, terrestrial planet, or failed core which has a major satellite is likely to have its obliquity stabilized by the presence of that satellite.
+# To select a value of the planet’s obliquity at random, roll 3d6. Add the same modifier that was computed during Step Nineteen for the Rotation Period Table, based 
+# on the degree of tidal deceleration applied by the major satellite (T times twelve, rounded to the nearest integer). Refer to the Obliquity Table."
+
+# Codify the Olibiquity Table
+
+# "Feel free to adjust a result from this procedure to any value between the next lower and next higher rows on the table.
+
+# "If the result is Extreme, the obliquity will be anywhere from about 50 degrees up to almost 90 degrees. To select a value at random, roll 1d6 on the 
+# Extreme Obliquity Table. Again, feel free to adjust a result from this procedure to any value between the next lower and next higher rows on the
+# table."
+
+# Codify the Extreme Obliquity Table
+
+# "Third Case: Planets Without Major Satellites"
+
+# "A leftover oligarch, terrestrial planet, or failed core which has no major satellite will be most affected by its primary star.
+# If a planet is in a spin-orbit resonance, its obliquity will be minimal. To determine its obliquity at random, assume a result of Minimal on the Obliquity Table, 
+# and roll 3d6-8 (minimum 0) to find the exact value."
+
+# "Otherwise, without the stabilizing presence of a major satellite, the planet’s obliquity is likely to change more drastically over time. Minor perturbations from 
+# other planets in the system may lead to chaotic “excursions” of the rotation axis. For example, although at present the obliquity of Mars is about 25 degrees, 
+# some models predict that Mars undergoes major excursions from about 0 degrees to as high as 60 degrees over millions of years."
+
+# "To select a value for obliquity at random, begin by rolling 3d6:"
+# "On a 7 or less, the planet’s obliquity has High Instability. Roll on the Obliquity Table with an additional -7 modifier."
+# "On a 8-13, the planet’s obliquity is not currently unstable. Roll on the Obliquity Table normally."
+# "On a 14 or higher, the planet’s obliquity has High Instability. Roll on the Obliquity Table with an additional +7 modifier."
+
+# "Make a note of any result indicating High Instability. The planet’s rotational axis is likely to be changing its orientation frequently across million-year 
+# timescales, and the result is likely to be drastic climate change."
+
+# "Now make a roll on the Obliquity Table. Apply the same modifier that was computed during Step Nineteen for the Rotation Period Table, based on the degree of tidal 
+# deceleration applied by the primary star (T times twelve, rounded to the nearest integer). Also apply any modifier (-7 or +7) due to High Instability. Refer to the 
+# Obliquity Table, and possibly the Extreme Obliquity Table, as required to determine the planet's current obliquity"
