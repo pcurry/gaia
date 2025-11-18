aaf4bbf76a3bb4604a4be2491c50a4ee8c85f848
tcdoggett <tcdoggett@gmail.com>
2025-11-18 17:10:48 -0500

    Update carbon_dioxide.py

--- src/planetary_system_data/carbon_dioxide.py
+++ src/planetary_system_data/carbon_dioxide.py
@@ -1,1 +1,51 @@
 # this sub-program covers step 28 ("Carbon Dioxide") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
+# starting from page 112
+
+# "This step estimates the initial amount of carbon dioxide (CO2) in the atmosphere."
+
+# "It appears likely that the atmospheres of some Earthlike worlds are subject to a “self-correcting” process, whereby carbon dioxide builds up in the atmosphere until surface
+# temperatures are high enough to allow for plenty of liquid water. After that point, carbon dioxide is increasingly removed from the atmosphere by processes of chemical weathering. The
+# net effect of this carbonate-silicate cycle is to maintain liquid-water temperatures on Earthlike worlds. To estimate the current level of carbon dioxide in a world’s atmosphere, this process
+# must be taken into account."
+
+# Procedure
+
+# "Check the following cases. Note that a Class 6 (Luna-type) world will have no significant atmosphere; in this case, skip ahead to Step Twenty-Nine."
+
+# Class 1 (Venus-Type) Worlds
+
+# "During a runaway dry greenhouse, the extreme heat “bakes” carbon dioxide out of the planet’s lithosphere in enormous quantities. This leads to an extremely hot and dense atmosphere
+# composed almost entirely of carbon dioxide. In this case, estimate the partial atmospheric mass of carbon dioxide (MCO2) as equal to 100 times the atmosphere retention factor. Vary this
+# value by up to 10% and round it to two significant figures. No carbonate-silicate cycle is possible, so there will be no significant native life. Skip ahead to Step Thirty."
+
+# Class 2 (Dulcinea-Type) and Class 3 (Titan-Type) Worlds
+
+# "We assume that carbon dioxide will not be a significant portion (more than about 1%) of the atmosphere for Class 2 worlds. The presence of carbon dioxide in such atmospheres will
+# have little effect on the local greenhouse effect, which will instead be dominated by molecular hydrogen. Meanwhile, Class 3 worlds are too cold for gaseous carbon dioxide. In either of these
+# cases, skip ahead to Step Twenty-Nine."
+
+# Class 4 (Earth-Type) and Class 5 (Mars-Type) Worlds
+
+# "For Class 4 worlds for which no runaway dry greenhouse has taken place, and for Class 5 worlds, the world will retain a significant amount of carbon dioxide if its M-number is no
+# greater than 44, its blackbody temperature is at least 195 K, and it has an atmosphere retention factor greater than 0. If this is the case, the world will have an initial partial atmospheric mass
+# of carbon dioxide (MCO2) equal to the atmosphere retention factor. Vary this value by up to 10% and round it to two significant figures."
+
+# "Note that for a young Class 4 world (say, well under a billion years old) the atmosphere may be dominated by methane and water vapor rather than carbon dioxide. For such a world, the
+# notional partial atmospheric mass of carbon dioxide serves as a projection for how much of the compound is likely to be present after any oceans form and the atmospheric methane is
+# chemically broken down."
+
+# "If the world develops an active carbonate-silicate cycle, much of this early carbon dioxide may be removed from the atmosphere over time."
+
+# "A Class 5 world with atmospheric retention factor of 0 may still retain a trace atmosphere of carbon dioxide, if it still meets the other requirements above (M-number and blackbody
+# temperature). To determine the partial atmospheric mass of carbon dioxide in this case, roll 1d6 and multiply by 0.01. Vary this value by up to 10% and round it to two significant figures."
+
+# "Carbonate-Silicate Cycle
+# "A world can only have an active carbonate-silicate cycle in effect if it retains more than a trace of carbon dioxide in the atmosphere, and it also has either Moderate or Extensive prevalence of
+# water. In practice, this will only occur for some Class 4 worlds. If those conditions hold, to determine whether a carbonate-silicate cycle is in effect, evaluate the following:
+
+# Codify Equation on page 113
+
+# "T is an estimate of the world’s average surface temperature assuming only the greenhouse effect of carbon dioxide, B is the blackbody temperature as determined in Step Twenty-Two, A
+# is the albedo as determined in Step Twenty-Seven, and MCO2 is the partial atmospheric mass of carbon dioxide as estimated above. Round T down to the next kelvin.
+
+# "An active carbonate-silicate cycle will exist on the world if and only if T is at least 260 K. Make a note if this is the case"
