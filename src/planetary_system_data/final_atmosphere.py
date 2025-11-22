e037ed1abda52fa10f21c5b55cc10d780d40f9a5
tcdoggett <tcdoggett@gmail.com>
2025-11-21 22:49:36 -0500

    Update final_atmosphere.py

--- src/planetary_system_data/final_atmosphere.py
+++ src/planetary_system_data/final_atmosphere.py
@@ -1,1 +1,67 @@
 # this sub-program covers step 31 ("Finalize Atmosphere") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
+# starting from page 124
+
+# "In this step, we finalize the world’s atmosphere, using data collected in prior steps. This determines the atmosphere's composition, standard pressure, and scale 
+# height. Scale height is a measure of how atmospheric pressure falls off with altitude."
+
+# Procedure
+
+# "Carry out the following for the world under consideration."
+
+# Total Atmospheric Mass
+# Collect the partial atmospheric mass of all the components estimated in previous steps:
+# Molecular hydrogen (MH2) as estimated in Step Twenty-Six
+# Helium (MHe) as estimated in Step Twenty-Six
+# Molecular nitrogen (MN2) as estimated in Step Twenty-Six
+# Carbon dioxide (MCO2) as first estimated in Step Twenty-Eight, and possibly adjusted in Step Thirty
+# Molecular oxygen (MO2) as estimated in Step Twenty-Nine
+# Water vapor (MH2O) as estimated in Step Thirty
+
+# "The sum of all these partial atmospheric masses, rounded to two significant figures, is the total atmospheric mass. Write it down; make a note of the presence of 
+# trace amounts of methane and ozone in the atmosphere, although they won’t contribute significantly to the total atmospheric mass."
+
+# Atmospheric Pressure
+
+# "The world’s atmospheric pressure at sea level (or its lowest elevation if no oceans are present) uses this formula:"
+
+# Codify Equation on page 124
+
+# "P is the atmospheric pressure, MA is the total atmospheric mass, and G is the world’s surface gravity. The result is measured in standard atmospheres. A world an 
+# with atmospheric pressure of 1 has the same pressure as Earth at sea level. A standard atmosphere is 101,325 pascals, or 1,013 millibars."
+
+# "This same procedure can be used to determine the partial pressure of each major atmospheric component at sea level. Multiply the partial atmospheric mass for that 
+# component by the world’s surface gravity."
+
+# Evaluating Human Breathability
+
+# "For humans to comfortably breathe an atmosphere, it must meet all the following criteria:
+# Molecular oxygen at a partial pressure of 0.120 to 0.300 atmospheres. Below this range, a respirator is required. Above the range, a filter or “reducing respirator” 
+# will be needed. High levels of oxygen will also encourage widespread fires.
+# Carbon dioxide at a partial pressure no higher than 0.015 atmospheres. Above this range, humans will suffer respiratory difficulties and may need a filter.
+# Molecular nitrogen at a partial pressure no higher than about 4 atmospheres. Above this range, humans will suffer from nitrogen narcosis and eventually lose 
+# consciousness. A filter or reducing respirator will be required.
+# The other likely components of a Class 4 world’s atmosphere are unlikely to have any toxic or suffocating effect, so long as the above criteria are all met."
+
+# Scale Height
+
+# "An atmosphere’s scale height is the standard measure of how atmospheric pressure decreases as a function of altitude. To estimate the scale height for a world, 
+# begin by evaluating the following:"
+
+# Codify Equation on page 125
+
+# "K is an average molecular mass for the world’s atmosphere, M is the total atmospheric mass, and the rest of the formula relies on the partial atmospheric masses for 
+# various components determined earlier. The formula for the scale height itself is:"
+
+# Codify Equation on page 125
+
+# "H is the atmosphere’s scale height in kilometers, T is the average surface temperature, K is theaverage molecular mass computed above, and G is the world’s surface 
+# gravity. The scale height can be used to estimate the atmospheric pressure at a given altitude:"
+
+# Codify Equation on page 125
+
+# "P is the pressure at the chosen altitude, P0 is the atmospheric pressure (or the partial pressure) at sea level, A is the altitude above that level in kilometers, 
+# and H is the scale height.
+
+# "This formula can be inverted to estimate the altitude at which the atmospheric pressure (or partial pressure) has a desired value:
+
+# Codify Equation on page 125
