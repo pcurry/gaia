f452b108e2dfd9afad198569c34dbc1b675d8eee
tcdoggett <tcdoggett@gmail.com>
2025-12-13 18:03:06 -0500

    refactoring

--- src/planetary_system_data/albedo.py
+++ src/planetary_system_data/albedo.py
@@ -1,25 +1,36 @@
-# this sub-program covers step 27 ("Albedo") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
-# starting from page 111
+# this sub-program covers step 27 ("Albedo") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for 
+# Interstellar Fiction" starting from page 111
 
-# "Several of the next steps support the determination of the world’s average surface temperature. This quantity is strongly dependent on the world’s blackbody 
-# temperature, established in Step Twenty-Two. However, worlds are not perfect thermal blackbodies, so this estimate must be adjusted."
+# "Several of the next steps support the determination of the world’s average surface temperature. This quantity is strongly 
+# dependent on the world’s blackbody temperature, established in Step Twenty-Two. However, worlds are not perfect thermal 
+# blackbodies, so this estimate must be adjusted."
 
-# "Some of the primary star’s energy input will be reflected away from the world’s surface, making no contribution to its heat budget. This factor is related to the 
-# world’s albedo, a measure of its reflectivity."
+# "Some of the primary star’s energy input will be reflected away from the world’s surface, making no contribution to its heat 
+# budget. This factor is related to the world’s albedo, a measure of its reflectivity."
 
 # Procedure
 
-# "To determine a world’s albedo at random, begin by referring to the Albedo Table. Roll 3d6, multiply the result by 0.01, and add it to the base albedo from the
-# table for the appropriate world class."
+# "To determine a world’s albedo at random, begin by referring to the Albedo Table. Roll 3d6, multiply the result by 0.01, and 
+# add it to the base albedo from the table for the appropriate world class."
+
+from dice import threeD6
+albedo_roll = threeD6()
+albedo_roll_modified = albedo_roll * 0.01
 
 # Codify the Albedo Table on page 111
 
-# "Class 6 worlds are a special case. Such a world lacks a significant atmosphere to form clouds, or to modify the color and reflectivity of the surface. It will
-# therefore tend to be dark, unless vulcanism or some similar process acts to renew its surface with brighter material."
+# "Class 6 worlds are a special case. Such a world lacks a significant atmosphere to form clouds, or to modify the color and 
+# reflectivity of the surface. It will therefore tend to be dark, unless vulcanism or some similar process acts to renew its 
+# surface with brighter material."
 
-# "Even Class 6 worlds with Massive water, covered with thick layers of ice, may have cryovolcanoes which constantly refresh the icy surface."
-# "For a Class 6 world, check to see whether it falls into any of the following cases. If so, add the indicated factor to the albedo."
+# "Even Class 6 worlds with Massive water, covered with thick layers of ice, may have cryovolcanoes which constantly refresh the 
+# icy surface."
+
+# "For a Class 6 world, check to see whether it falls into any of the following cases. If so, add the indicated factor to the 
+# albedo."
 
 # "Add 0.5 if the world has a Molten or Soft lithosphere."
-# "Add 0.3 if the world has an Early or Mature Plate lithosphere, or if it has an Ancient Plate lithosphere with Mobile plate tectonics."
-# "Add 0.3 if the world has an Ancient Plate lithosphere with Fixed plate tectonics, or it has a Solid lithosphere, and its blackbody temperature is less than 80 K."
+# "Add 0.3 if the world has an Early or Mature Plate lithosphere, or if it has an Ancient Plate lithosphere with Mobile plate
+#  tectonics."
+# "Add 0.3 if the world has an Ancient Plate lithosphere with Fixed plate tectonics, or it has a Solid lithosphere, and its 
+# blackbody temperature is less than 80 K."
