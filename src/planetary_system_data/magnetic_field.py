d5a006ea65e47ae5c0319b7ea5448f22d0b114ec
tcdoggett <tcdoggett@gmail.com>
2025-12-22 22:35:46 -0500

    Update magnetic_field.py

--- src/planetary_system_data/magnetic_field.py
+++ src/planetary_system_data/magnetic_field.py
@@ -1,22 +1,31 @@
-# this sub-program covers step 25 ("Magnetic Field") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction" - starting from page 107
+# this sub-program covers step 25 ("Magnetic Field") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for 
+# Interstellar Fiction" - starting from page 107
 
 # "This step estimates whether the world under development has a significant magnetic field."
 
-# "The possible cases for a world’s magnetic field will be sorted into four categories: None, Weak, Moderate, and Strong, defined as follows."
-# "None: The world has no detectable magnetic field and is completely unprotected from the stellar wind. Examples: Venus, Earth’s moon, Mars, most of the gas giant planets’ major satellites."
-# "Weak: The world has a detectable magnetic field (about 1% as strong as Earth’s), but it offers no significant protection from the stellar wind. Examples: Mercury or Ganymede."
-# "Moderate: The world’s magnetic field is strong enough to offer limited protection against the stellar wind (about 10% as strong as Earth’s). Examples: None in our planetary system."
-# "Strong: The world’s magnetic field is at least comparable to that of Earth, sufficient to provide adequate protection against the stellar wind. Examples: Earth, the gas giant planets."
+# "The possible cases for a world’s magnetic field will be sorted into four categories: None, Weak, Moderate, and Strong, defined 
+# as follows."
+# "None: The world has no detectable magnetic field and is completely unprotected from the stellar wind. Examples: Venus, Earth’s 
+# moon, Mars, most of the gas giant planets’ major satellites."
+# "Weak: The world has a detectable magnetic field (about 1% as strong as Earth’s), but it offers no significant protection from 
+# the stellar wind. Examples: Mercury or Ganymede."
+# "Moderate: The world’s magnetic field is strong enough to offer limited protection against the stellar wind (about 10% as strong 
+# as Earth’s). Examples: None in our planetary system."
+# "Strong: The world’s magnetic field is at least comparable to that of Earth, sufficient to provide adequate protection against 
+# the stellar wind. Examples: Earth, the gas giant planets."
 
-# "A world’s magnetic field seems to depend on several items. First, the world needs to have a hot, liquid outer core of significant mass, composed largely of iron. Second, there must be
-# convection taking place in that iron outer core, causing rising and falling currents. Finally, the world must rotate."
+# "A world’s magnetic field seems to depend on several items. First, the world needs to have a hot, liquid outer core of 
+# significant mass, composed largely of iron. Second, there must be convection taking place in that iron outer core, causing 
+# rising and falling currents. Finally, the world must rotate."
 
-# "If all three of these conditions hold, the iron outer core forms a dynamo which creates a significant magnetic field. This, in turn, helps protect the world’s atmosphere from being
-# stripped away by stellar wind, and it also protects the surface of the world from some harmful radiation. In our own planetary system, only Earth and the gas giant planets have strong
-# magnetic fields."
+# "If all three of these conditions hold, the iron outer core forms a dynamo which creates a significant magnetic field. This, in 
+# turn, helps protect the world’s atmosphere from being stripped away by stellar wind, and it also protects the surface of the 
+# world from some harmful radiation. In our own planetary system, only Earth and the gas giant planets have strong magnetic 
+# fields."
 
-# "The third condition (that the world must rotate) is almost universal. Even a tide-locked world still rotates, and physical modeling suggests that even slow rotation is enough to support a
-# working dynamo. The existence of strong convective heat transfer through a world’s outer core is the critical factor."
+# "The third condition (that the world must rotate) is almost universal. Even a tide-locked world still rotates, and physical 
+# modeling suggests that even slow rotation is enough to support a working dynamo. The existence of strong convective heat 
+# transfer through a world’s outer core is the critical factor."
 
 # "Procedure"
 
