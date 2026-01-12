df98413b7b5381bbc1fedb52dcdf05bb96ddfb3b
tcdoggett <tcdoggett@gmail.com>
2026-01-11 19:35:21 -0500

    Update orbital_period.py

--- src/planetary_system_data/orbital_period.py
+++ src/planetary_system_data/orbital_period.py
@@ -1,7 +1,8 @@
-# this sub-program covers step 18 ("Orbital Period") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction" - starting from pg. 89
+# this sub-program covers step 18 ("Orbital Period") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for 
+# Interstellar Fiction" - starting from pg. 89
 
-# "The orbital period of any object is strictly determined by the total mass of the system and the radius of the object’s orbit. This is one of the earliest results in 
-# modern astronomy, dating back to Kepler’s third law of planetary motion (1619)"
+# "The orbital period of any object is strictly determined by the total mass of the system and the radius of the object’s 
+# orbit. This is one of the earliest results in modern astronomy, dating back to Kepler’s third law of planetary motion (1619)"
 
 # Procedure
 
@@ -13,8 +14,9 @@
 
 # Codify Equation on pg. 89
 
-# "T is the orbital period in hours, D is the radius of the satellite’s orbit in kilometers, and M<sub>P</sub> and M<sub>S</sub> are the masses of the planet and the 
-# satellite, in Earth-masses. If the satellite is a moonlet, assume its mass is negligible compared to its planet and use a value of zero for M<sub>S</sub>."
+# "T is the orbital period in hours, D is the radius of the satellite’s orbit in kilometers, and M<sub>P</sub> and 
+# M<sub>S</sub> are the masses of the planet and the satellite, in Earth-masses. If the satellite is a moonlet, assume its 
+# mass is negligible compared to its planet and use a value of zero for M<sub>S</sub>."
 
 # Second Case: Planets
 
@@ -22,5 +24,6 @@
 
 # Codify Equation on pg. 90
 
-# "T is the orbital period in hours, D is the radius of the planet’s orbit in AU, and M is the mass of the primary star in solar masses. Planets have negligible mass 
-# compared to their primary stars, at least at the degree of precision offered by this equation, and so don’t need to be included in the calculation."
+# "T is the orbital period in hours, D is the radius of the planet’s orbit in AU, and M is the mass of the primary star in 
+# solar masses. Planets have negligible mass compared to their primary stars, at least at the degree of precision offered 
+# by this equation, and so don’t need to be included in the calculation."
