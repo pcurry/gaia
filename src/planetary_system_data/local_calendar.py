8ee3c3af7aa926b9195b2ae15f9309467dc1268e
tcdoggett <tcdoggett@gmail.com>
2026-01-16 18:23:23 -0500

    Update local_calendar.py

--- src/planetary_system_data/local_calendar.py
+++ src/planetary_system_data/local_calendar.py
@@ -1,54 +1,62 @@
-# this sub-program covers step 21 ("Local Calendar") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for Interstellar Fiction"
-# starting from page 95
+# this sub-program covers step 21 ("Local Calendar") from Zeigler, 2024 - "Architect of Worlds: Comprehensive World Design for 
+# Interstellar Fiction" starting from page 95
 
-# "This step determines elements of the local calendar on the world being developed: the length of the local day, the length of any “month” determined by a 
-# major satellite, and so on."
+# "This step determines elements of the local calendar on the world being developed: the length of the local day, the length 
+# of any “month” determined by a major satellite, and so on."
 
 # Length of Local Day for a Planet
 
-# "To determine the length of a planet’s day—the planet’s rotation period with respect to its primary star rather than with respect to the distant stars—compute 
+# "To determine the length of a planet’s day—the planet’s rotation period with respect to its primary star rather than with 
+# respect to the distant stars—compute 
 # the following:"
 
 # codify equation on page 95
 
-# "T is the apparent length of the planet’s day in hours, P is the planet’s orbital period in hours as determined in Step Eighteen, while R is the planet’s 
-# rotational period in hours as determined in Step Nineteen."
+# "T is the apparent length of the planet’s day in hours, P is the planet’s orbital period in hours as determined in Step 
+# Eighteen, while R is the planet’s rotational period in hours as determined in Step Nineteen."
 
-# "This equation is undefined in cases when the orbital period and rotational period are equal (that is, the planet is tide-locked or in a spin-orbital resonance of 1:1). 
-# In this case, the length of the local day is effectively infinite—the sun never moves far in the sky!"
+# "This equation is undefined in cases when the orbital period and rotational period are equal (that is, the planet is 
+# tide-locked or in a spin-orbital resonance of 1:1). In this case, the length of the local day is effectively infinite—
+# the sun never moves far in the sky!"
 
-# "At the other extreme, if the orbital period is much longer than the rotational period, then the day length and the rotational period will be very close together.
-# To determine the length of the local year in local days, simply divide the planet’s orbital period by the length of the local day as computed above."
+# "At the other extreme, if the orbital period is much longer than the rotational period, then the day length and the 
+# rotational period will be very close together. To determine the length of the local year in local days, simply divide the 
+# planet’s orbital period by the length of the local day as computed above."
 
 # "Length of Apparent Orbital Period for a Satellite"
 
-# "To determine the length of a satellite’s apparent orbital period, from any position on the planet’s surface, use the same equation:"
+# "To determine the length of a satellite’s apparent orbital period, from any position on the planet’s surface, use the same 
+# equation:"
 
 # codify equation on page 95
 
-# "In this case, T is the apparent length of the satellite’s orbital period in hours, P is the satellite’s actual orbital period in hours as determined in 
-# Step Eighteen, while R is the planet’s rotational period in hours as determined in Step Nineteen."
+# "In this case, T is the apparent length of the satellite’s orbital period in hours, P is the satellite’s actual orbital 
+# period in hours as determined in Step Eighteen, while R is the planet’s rotational period in hours as determined in Step 
+# Nineteen."
 
-# "Again, this equation is undefined in cases when the satellite’s orbital period and the planet’s rotational period are equal (that is, the planet is tide-locked 
-# to its satellite, or the satellite happens to orbit at a geosynchronous distance). In this case, the length of the satellite’s apparent orbital period is effectively 
-# infinite—the satellite never moves in the sky."
+# "Again, this equation is undefined in cases when the satellite’s orbital period and the planet’s rotational period are equal 
+# (that is, the planet is tide-locked to its satellite, or the satellite happens to orbit at a geosynchronous distance). In 
+# this case, the length of the satellite’s apparent orbital period is effectively infinite—the satellite never moves in the 
+# sky."
 
-# "At the other extreme, if the satellite’s orbital period is much longer than the planet’s rotationalvperiod, then the apparent orbital period and the rotational 
-# period will be very close together. Earth’s moon is a familiar example—its apparent motion in the sky is dominated by Earth’s rotation."
+# "At the other extreme, if the satellite’s orbital period is much longer than the planet’s rotationalvperiod, then the 
+# apparent orbital period and the rotational period will be very close together. Earth’s moon is a familiar example—its 
+# apparent motion in the sky is dominated by Earth’s rotation."
 
-# "It’s possible for a satellite’s orbital period to be shorter than the planet’s rotational period. For example, a close-orbiting moonlet is likely to fall into 
-# this case. The apparent orbital period will therefore be negative, indicating that the satellite appears to move backward over time. The satellite will rise in 
-# the west and set in the east."
+# "It’s possible for a satellite’s orbital period to be shorter than the planet’s rotational period. For example, a 
+# close-orbiting moonlet is likely to fall into this case. The apparent orbital period will therefore be negative, indicating 
+# that the satellite appears to move backward over time. The satellite will rise in the west and set in the east."
 
 # Length of Synodic Month for a Satellite
 
-# "To determine the length of a satellite’s synodic month (that is, how long it takes for the satellite to return to the same phase in the sky), use the same 
-# equation once more:"
+# "To determine the length of a satellite’s synodic month (that is, how long it takes for the satellite to return to the same 
+# phase in the sky), use the same equation once more:"
 
 # codify equation on page 96
 
-# "In this case, T is the length of the satellite’s synodic month in hours, P is the planet’s orbital period in hours as determined in Step Eighteen, while R is the 
-# satellite’s orbital period in hours as determined in Step Eighteen."
+# "In this case, T is the length of the satellite’s synodic month in hours, P is the planet’s orbital period in hours as 
+# determined in Step Eighteen, while R is the satellite’s orbital period in hours as determined in Step Eighteen."
 
-# "It’s unlikely for a satellite to have the same or longer orbital period around its planet as the planet does around its primary star, so the undefined or 
-# negative cases will not occur. T will indicate the period between (e.g.) one “full moon” and the next, as observed from the planet’s surface"
+# "It’s unlikely for a satellite to have the same or longer orbital period around its planet as the planet does around its 
+# primary star, so the undefined or negative cases will not occur. T will indicate the period between (e.g.) one “full moon” 
+# and the next, as observed from the planet’s surface"
